d17414bea75a755a88f14bb1c4e0b6fd8dd1df49
spacetimeengineer3 <erwibben@gmail.com>
2021-03-18 22:00:17 -0500

    Added the ability to go ddeeper

--- headtest.py
+++ headtest.py
@@ -9,16 +9,18 @@ soup = BeautifulSoup(url.text, features='html.parser')
 
 
 tags = soup.find_all('a')
-
+#print(tags)
 links = []
+goodlinks = []
 for tag in tags:
     links.append(tag.get('href'))
+
 #for link in links:
-   # print(link)
+    #print(link)
 
 def testlinks():
     for link in links:
-        #print(link)
+        #print(links)
         try:
             if "http" not in link:
                 #print("NO HTTP")
@@ -31,12 +33,33 @@ def testlinks():
 
 
             if r.status_code not in [200, 302]:
-                print("%s is a bad Link" %link)
+                #print("%s is a bad Link" %link)
+                #links.pop(link)
+                #print(r.status_code)
+                continue
         except:
-            print("%s is a bad EXECEPT Link" %link)
+            #print("%s is a bad Link" %link)
+            #links.remove(link)
+            continue
+        else:
+            goodlinks.append(link)
 
 testlinks()
 
+for goodlink in goodlinks:
+    if "http" not in goodlink:
+        #print("NO HTTP")
+        if goodlink[0] == "/":
+            r = requests.get('http://10.92.21.107:8080' + goodlink)
+        if goodlink[0] != "/":
+            r = requests.get('http://10.92.21.107:8080/' + goodlink)
+    else:
+        r = requests.get(goodlink)
+    soup = BeautifulSoup(r.text, features='html.parser')
+    tags = soup.find_all('a')
+    print(tags)
+    #for link in links:
+    #print(link)
 #for child in soup.descendants:
     #print(child)
 
